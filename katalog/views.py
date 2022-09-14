975d78f5715855a5af3fe17e0daaf7d5df613d19
Jeremy Alva Prathama <jeremyalvax@gmail.com>
2022-09-14 10:32:26 +0700

    finished tugas 01

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_list_katalog = CatalogItem.objects.all()
+    response = {
+        'nama': 'Jeremy Alva Prathama',
+        'npm': '2106640354',
+        'list_katalog': data_list_katalog,
+    }
+    return render(request, "katalog.html", response)
