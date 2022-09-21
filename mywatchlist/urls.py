940a6d15a1e21bf0e6a0b13425eb44076f71abf1
Jeremy Alva Prathama <jeremyalvax@gmail.com>
2022-09-21 23:25:53 +0700

    fixed data for app

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from mywatchlist.views import show_mywatchlist, retrieve_html, retrieve_html_by_id, retrieve_json, retrieve_json_by_id, retrieve_xml, retrieve_xml_by_id
+from mywatchlist.views import show_mywatchlist, retrieve_html, retrieve_json, retrieve_xml
 
 app_name = 'mywatchlist'
 
@@ -8,7 +8,4 @@ urlpatterns = [
     path('html/', retrieve_html, name='retrieve_html'),
     path('xml/', retrieve_xml, name='retrieve_xml'),
     path('json/', retrieve_json, name='retrieve_json'),
-    path('html/<int:id>', retrieve_html_by_id, name='retrieve_html_by_id'),
-    path('xml/<int:id>', retrieve_xml_by_id, name='retrieve_xml_by_id'),
-    path('json/<int:id>', retrieve_json_by_id, name='retrieve_json_by_id'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render
-from django.template.loader import render_to_string
 from mywatchlist.models import MyWatchList
 
 
@@ -31,25 +30,3 @@ def retrieve_json(request):
     data = MyWatchList.objects.all()
 
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-# Retrieve by id
-def retrieve_html_by_id(request, id):
-    data_watchlist = MyWatchList.objects.filter(pk=id)
-
-    response = {
-        'mywatchlist': data_watchlist,
-    }
-
-    return render(request, "watchlist_html.html", response)
-
-def retrieve_xml_by_id(request, id):
-    data = MyWatchList.objects.filter(pk=id)
-
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-
-def retrieve_json_by_id(request, id):
-    data = MyWatchList.objects.filter(pk=id)
-
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-
